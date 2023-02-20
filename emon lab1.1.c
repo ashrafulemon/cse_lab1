4f99803c268bb3782b027ab4b8676ff70654b88c
Ashraful Islam Emon <emon118.bd@gmail.com>
2023-02-20 14:10:42 +0600

    Update emon lab1.1.c

--- emon lab1.1.c
+++ emon lab1.1.c
@@ -3,9 +3,10 @@ int main()
 {
     int x,y;
     scanf("%d %d",&x,&y);
-    printf("sum= %d\n",x+y);
-    printf("sub= %d\n",x-y);
-    printf("mul= %d\n",x*y);
-    printf("div= %f\n",(float)x/y);
+    printf("addition       = %d\n",x+y);
+    printf("subtraction    = %d\n",x-y);
+    printf("division       = %f\n",(float)x/y);
+    printf("multiplication = %d\n",x*y);
+    printf("modulus        = %d\n",x%y);
     return 0;
 }
